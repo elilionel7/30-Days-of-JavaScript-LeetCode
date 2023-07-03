3c4e5b4fa7623b47b2ef5b9b0a74b28bb13cabc8
elilionel7 <elilionel7@gmail.com>
2023-07-02 19:25:43 -0500

    createCounter.js

---
+++ Closures/createCounter.js
@@ -0,0 +1,29 @@
+/**
+ * 2620. Counter (Easy)
+Given an integer n, return a counter function.
+This counter function initially returns n
+and then returns 1 more than the previous value
+every subsequent time it is called (n, n + 1, n + 2, etc).
+ */
+
+/**
+ * @param {number} new
+ * @return {function} counter
+ */
+function createCounter(n) {
+    return function() {
+        return n++;
+    };
+};
+
+
+
+
+
+// Example
+
+let counter = createCounter(1);
+
+console.log(counter()); // 2
+console.log(counter()); // 3
+console.log(counter()); // 4
