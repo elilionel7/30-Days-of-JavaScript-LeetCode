f68eb16c6c71a2ce33291bdace65b5dd7aa55a4a
elilionel7 <elilionel7@gmail.com>
2023-07-23 15:24:55 -0500

    complete join.js

--- Json/join.js
+++ Json/join.js
@@ -1,9 +1,10 @@
 // 2722. Join Two Arrays by ID
-// Medium
-// 40
-// 9
-// Companies
-// Given two arrays arr1 and arr2, return a new array joinedArray. All the objects in each of the two inputs arrays will contain an id field that has an integer value. joinedArray is an array formed by merging arr1 and arr2 based on their id key. The length of joinedArray should be the length of unique values of id. The returned array should be sorted in ascending order based on the id key.
+// Given two arrays arr1 and arr2, return a new array joinedArray.
+//All the objects in each of the two inputs arrays will contain
+//an id field that has an integer value. joinedArray is an array
+//formed by merging arr1 and arr2 based on their id key.
+//The length of joinedArray should be the length of unique values of id.
+//The returned array should be sorted in ascending order based on the id key.
 
 // If a given id exists in one array but not the other, the single object with that id should be included in the result array without modification.
 
@@ -19,11 +20,9 @@
 var join = function(arr1, arr2) {
     const mergedMap = {};
 
-
     for (const obj1 of arr1) {
         mergedMap[obj1.id] = { ...obj1 };
     }
-
 
     for (const obj2 of arr2) {
         if (mergedMap[obj2.id]) {
@@ -35,18 +34,13 @@ var join = function(arr1, arr2) {
         }
     }
 
+    const result = Object.values(mergedMap);
+    result.sort((a, b) => a.id - b.id);
 
-    //const result = Object.values(mergedMap);
-
-
-    //result.sort((a, b) => a.id - b.id);
-
-    return mergedMap;
+    return result;
 };
 
 // this examples are from leetCode
-
-
 Input:
 arr1 = [
     {"id": 1, "x": 2, "y": 3},
